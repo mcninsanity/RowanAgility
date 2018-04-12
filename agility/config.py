8cf9867d2c5678d9d7941d07cf695702bfb654a8
xian14 <jeku_15@live.com>
2018-04-12 13:00:33 -0400

    Removed agility folder from repository

--- agility/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config(object):
-    # change it as needed
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/senior_project'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
