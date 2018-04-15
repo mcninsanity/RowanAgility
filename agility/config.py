1d86e43e90e0137939e602369c3cfe1fa8e808f0
mcninsanity <newman22@students.rowan.edu>
2018-04-15 11:25:10 -0400

    Updates all the ids and some other quality of life stuff

--- agility/config.py
+++ agility/config.py
@@ -3,5 +3,5 @@ import os
 class Config(object):
     # change it as needed
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/senior_project'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:Ytiliga2018**@localhost/senior_project'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
